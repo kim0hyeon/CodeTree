820913aa993ecd1456aec4bbdd593054843caca2
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-03-02 22:47:12 +0900

    Title: 기수 정렬 구현, Time: 40ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 250302/기수 정렬 구현/implement-radix-sort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+int arr[100000];
+
+void RadixSort(){
+    int p = 1;
+    for (int pos = 0; pos < 6; pos++){
+        vector<int> arr_new[10];
+        for (int i = 0; i < n; i++){
+            int digit = (arr[i] / p) % 10;
+            arr_new[digit].push_back(arr[i]);
+        }
+
+        int index = 0;
+        for (int i = 0; i < 10; i++){
+            for (int j = 0; j < (int)arr_new[i].size(); j++){
+                arr[index++] = arr_new[i][j];
+            }
+        }
+    
+        p *= 10;
+    }
+}
+
+int main() {
+    
+    cin >> n;
+
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    RadixSort();
+
+    for (int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
