739a2f741d204449e15be929a9da0e03f28f59ac
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-03-07 14:35:18 +0900

    Title: 금 채굴하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250307/금 채굴하기/gold-mining.cpp
+++ 250307/금 채굴하기/gold-mining.cpp
@@ -8,10 +8,12 @@
 using namespace std;
 
 int find_gold(vector<vector<int>> &grid, int x, int y, int k, int price){
-    if (x-k < 0 || y-k < 0 || x+k >= grid.size() || y+k >= grid[0].size()) return 0; // 범위를 벗어나는 경우 0 리턴
+    if (x+k > grid.size() && y+k > grid[0].size() && x-k < 0 && y-k < 0) return 0;  // 사각형을 모두 채운 경우 더 이상 진행할 필요가 없으므로 벗어난다.
 
-    int count = 0;
+
+    int count = 0, mining = 0;
     int row = 0;  // 0행부터 시작
+
     for (int i = x-k; i <= x+k; i++){
         int skip = abs(row-k);  // 각 행마다 이루어져야 할 스킵의 개수
         int check = (k * 2 + 1) - (skip * 2);  // 각 행마다 채크해야할 칸의 개수
@@ -21,16 +23,23 @@ int find_gold(vector<vector<int>> &grid, int x, int y, int k, int price){
                 skip--;
                 continue;
             }
+
+            if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size()){ // 범위를 벗어나는 경우 스킵한다.
+                if (skip == 0) check--;  // 채굴해야 할 타이밍인데 넘어간 경우 채굴을 못하게 되니까 check-1 을 해야한다.
+                continue;
+            }
+
             if (check > 0){
                 count += grid[i][j];  // 해당 칸에 금이 있다면 +1이 자동으로 되고, 없다면 0을 더하기때문에 값이 유지된다.
                 check--;  // 체크 한번 했으니 체크해야할 개수 -1
+                mining++;  // 채굴을 했으니까 + 1
                 continue;
             }
         }
         row++;  // 다음 행으로 넘어갔으니 +1
     }
 
-    int total = (count * price) - (pow(k,2) + pow(k+1, 2));  // 총계를 계산한다.
+    int total = (count * price) - (pow(k,2) + pow(k+1,2));  // 총계를 계산한다.
 
     // 총계가 0 미만이면 손해를 보는 경우이므로 제외시켜야해서 count대신에 0을 넣는다.
     if (total < 0)
