7b3f1d6237d4bc9d3b8324984279755c22ce2b02
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-03-08 21:17:51 +0900

    Title: 삼각형 컨베이어 벨트, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 250308/삼각형 컨베이어 벨트/conveyor-belt-triangle.cpp
+++ 250308/삼각형 컨베이어 벨트/conveyor-belt-triangle.cpp
@@ -2,58 +2,46 @@
 
 using namespace std;
 
-
 int main(){
     int n, t;
     cin >> n >> t;
     int arr1[n], arr2[n], arr3[n];
-    for (int i = 0; i < n; i++)
-        cin >> arr1[i];
-    for (int i = 0; i < n; i++)
-        cin >> arr2[i];
-    for (int i = 0; i < n; i++)
-        cin >> arr3[i];
+
+    for (int i = 0; i < n; i++) cin >> arr1[i];
+    for (int i = 0; i < n; i++) cin >> arr2[i];
+    for (int i = 0; i < n; i++) cin >> arr3[i];
 
     while (t > 0){
-        int tmp = arr1[n-1];
-        for (int i = n; i >= 1; i--){
+        int tmp1 = arr1[n-1];  // arr1 마지막 원소 저장
+        int tmp2 = arr2[n-1];  // arr2 마지막 원소 저장
+        int tmp3 = arr3[n-1];  // arr3 마지막 원소 저장
+
+        // arr1 한 칸씩 이동
+        for (int i = n-1; i > 0; i--){
             arr1[i] = arr1[i-1];
         }
-        
-        arr1[0] = arr3[n-1];
+        arr1[0] = tmp3;  // arr1의 처음 값은 arr3의 마지막 원소
 
-        int tmp2 = arr2[n-1];
-
-        for (int i = n; i >= 1; i--){
+        // arr2 한 칸씩 이동
+        for (int i = n-1; i > 0; i--){
             arr2[i] = arr2[i-1];
         }
+        arr2[0] = tmp1;  // arr2의 처음 값은 arr1의 마지막 원소
 
-        arr2[0] = tmp;
-
-        for (int i = n; i >= 1; i--){
+        // arr3 한 칸씩 이동
+        for (int i = n-1; i > 0; i--){
             arr3[i] = arr3[i-1];
         }
-
-        arr3[0] = tmp2;
+        arr3[0] = tmp2;  // arr3의 처음 값은 arr2의 마지막 원소
 
         t--;
     }
 
-    for (int i = 0; i < n; i++){
-        cout << arr1[i] << " ";
-    }
-
+    for (int i = 0; i < n; i++) cout << arr1[i] << " ";
     cout << endl;
-
-    for (int i = 0; i < n; i++){
-        cout << arr2[i] << " ";
-    }
-
+    for (int i = 0; i < n; i++) cout << arr2[i] << " ";
     cout << endl;
-
-    for (int i = 0; i < n; i++){
-        cout << arr3[i] << " ";
-    }
+    for (int i = 0; i < n; i++) cout << arr3[i] << " ";
 
     return 0;
 }
