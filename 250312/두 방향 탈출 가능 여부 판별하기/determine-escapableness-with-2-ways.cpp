d4fb29801d9f63177c8d28157ff82a75a21fd771
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-03-12 18:06:38 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 250312/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
+++ 250312/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -3,28 +3,39 @@
 
 using namespace std;
 
-
 int n, m;
 vector<vector<int>> grid;
+vector<vector<int>> memo;  // -1: 아직 계산하지 않음, 0: 도착 불가능, 1: 도착 가능
 
-bool exist = false;
-
-void DFS_exit(int x, int y){
+bool DFS_exit(int x, int y) {
+    // 목적지에 도착하면 true 리턴
     if (x == n && y == m)
-        exist = true;
+        return true;
     
-    // 수행시간을 고려해 만약 exist가 발견되었다면 더 이상 함수 진행할 필요 없음!
-    if (x < n && grid[x+1][y] == 1 && !exist){  // x가 범위를 벗어나지 않고 다음 행이 진입 가능할 때
-        DFS_exit(x+1, y);
+    // 이미 계산한 경우 저장된 값을 리턴
+    if (memo[x][y] != -1)
+        return memo[x][y];
+    
+    bool canReach = false;
+    
+    // 아래쪽으로 이동 가능한 경우
+    if (x < n && grid[x+1][y] == 1) {
+        canReach = DFS_exit(x+1, y);
     }
-    if (y < m && grid[x][y+1] == 1 && !exist){  // y가 범위를 벗어나지 않고 다음 행이 진입 가능할 때
-        DFS_exit(x, y+1);
+    
+    // 만약 아래쪽에서 목적지에 도달하지 못하면 오른쪽으로 이동 시도
+    if (!canReach && y < m && grid[x][y+1] == 1) {
+        canReach = DFS_exit(x, y+1);
     }
+    
+    memo[x][y] = canReach;  // 결과 저장
+    return canReach;
 }
 
 int main(){
     cin >> n >> m;
     grid = vector<vector<int>>(n+1, vector<int>(m+1));
+    memo = vector<vector<int>>(n+1, vector<int>(m+1, -1)); // -1로 초기화
 
     for (int i = 1; i <= n; i++){
         for (int j = 1; j <= m; j++){
@@ -32,10 +43,7 @@ int main(){
         }
     }
 
-    DFS_exit(1, 1);
-
-    if (exist) cout << 1;
-    else cout << 0;
+    cout << (DFS_exit(1, 1) ? 1 : 0);
 
     return 0;
 }
