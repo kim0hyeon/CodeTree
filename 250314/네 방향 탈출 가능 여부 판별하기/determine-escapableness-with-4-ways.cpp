a40f16318e28ab173f4b54d480d69defe30c9481
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-03-14 21:10:41 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 250314/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
+++ 250314/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
@@ -1,36 +1,37 @@
 #include <iostream>
 #include <vector>
-
+#include <queue>
 using namespace std;
 
 int n, m;
 vector<vector<int>> grid;
 vector<vector<bool>> visited;
+queue<pair<int, int>> q;
+
 bool finds = false;
 
-void BFS_search(int x, int y){
-    if (x == n && y == m) {
-        finds = true;
-        return;
-    }
+void BFS_search(){
+    while (!q.empty()){
+        pair<int, int> cur = q.front();
+        int x = cur.first;
+        int y = cur.second;
+        q.pop();
 
-    visited[x][y] = true;
+        int dx[] = {-1, 0, 1, 0};
+        int dy[] = {0, 1, 0, -1};
 
-    int dx[] = {-1,0,1,0};
-    int dy[] = {0,1,0,-1};
+        for (int i = 0; i < 4; i++){
+            int tx = x + dx[i];
+            int ty = y + dy[i];
 
-    for (int i = 0; i < 4; i++){
-        int tx = x + dx[i];
-        int ty = y + dy[i];
-
-        if (tx > 0 && tx <= n && ty > 0 && ty <= m){
-            if (grid[tx][ty] == 1 && !visited[tx][ty]){
-                BFS_search(tx, ty);
+            if (tx > 0 && tx <= n && ty > 0 && ty <= m){
+                if (grid[tx][ty] == 1 && !visited[tx][ty]){
+                    q.push(make_pair(tx,ty));
+                    visited[tx][ty] = true;
+                }
             }
         }
     }
-
-    
 }
 
 int main(){
@@ -44,9 +45,12 @@ int main(){
         }
     }
 
-    BFS_search(1, 1);
+    q.push(make_pair(1, 1));
+    visited[1][1] = true;
 
-    finds ? cout << 1 : cout << 0;
+    BFS_search();
+
+    cout << visited[n][m];
 
     return 0;
 }
