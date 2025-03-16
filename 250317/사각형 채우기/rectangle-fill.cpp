1925eff0b1218d589d585a463cee973b5149c430
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-03-17 08:44:37 +0900

    Title: 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250317/사각형 채우기/rectangle-fill.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<int> grid(n+1);
+
+    grid[0] = 0;
+    grid[1] = 1;
+    grid[2] = 2;
+
+    for (int i = 3; i <= n; i++){
+        grid[i] = grid[i-1] + grid[i-2];
+    }
+
+    cout << grid[n];
+
+
+    return 0;
+}
+
+// 4일땐?
+// 1,1,1,1 / 2,1,1 / 1,1,2 / 1,2,1 / 2,2
