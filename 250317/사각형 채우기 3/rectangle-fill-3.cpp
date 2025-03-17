b230c484fa2479b4f9244f175ba750b5b0dc8884
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-03-17 17:17:19 +0900

    Title: 사각형 채우기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250317/사각형 채우기 3/rectangle-fill-3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+const long MOD = 1000000007;
+
+int main(){
+    int n;
+    cin >> n;
+
+    // n이 0 또는 1인 경우를 미리 처리
+    if(n == 0) {
+        cout << 1; 
+        return 0;
+    }
+    if(n == 1) {
+        cout << 2; 
+        return 0;
+    }
+
+    long arr[n+1];
+    arr[0] = 1; // 2x0 보드: 빈 타일링 1가지
+    arr[1] = 2; // 2x1 보드: 2가지 방법
+    arr[2] = 7; // 2x2 보드: 7가지 방법 (문제에서 주어진 값)
+    arr[3] = 22;
+
+    for (int i = 4; i <= n; i++){
+        arr[i] = (arr[i-1] * 2 + arr[i-2] * 3 + arr[i-3]*2 - arr[i-4]) % MOD;
+    }
+
+    cout << arr[n];
+
+    return 0;
+}
