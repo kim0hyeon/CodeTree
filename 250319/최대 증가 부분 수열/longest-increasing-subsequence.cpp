18f60efff3cbba381398858f213f9948cd0cb305
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-03-19 09:26:56 +0900

    Title: 최대 증가 부분 수열, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 250319/최대 증가 부분 수열/longest-increasing-subsequence.cpp
+++ 250319/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -9,11 +9,9 @@ int main(){
     cin >> n;
 
     vector<int> arr(n);
-    vector<int> dp(n, 0);
+    vector<int> dp(n, 1);
 
     for (int i = 0; i < n; i++) cin >> arr[i];
-
-    dp[0] = 1;
 
     for (int i = 1; i < n; i++){
         for (int j = 0; j < i; j++){
