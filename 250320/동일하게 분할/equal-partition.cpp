363270695fc84082f7c8ba8ab8628a69388c17ac
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-03-20 20:15:49 +0900

    Title: 동일하게 분할, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250320/동일하게 분할/equal-partition.cpp
+++ 250320/동일하게 분할/equal-partition.cpp
@@ -23,7 +23,7 @@ int main(){
 
     bool flag = false;
 
-    for (int i = 1; i < n; i++){
+    for (int i = 1; i <= n; i++){
         for (int j = 0; j < i; j++){
             if (dp[j] + vec[i] == total/2){
                 flag = true;
