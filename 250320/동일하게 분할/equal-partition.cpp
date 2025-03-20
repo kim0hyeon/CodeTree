140377ca31eff68d8a95893e3c82baf7a8c7bbdf
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-03-20 20:30:53 +0900

    Title: 동일하게 분할, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250320/동일하게 분할/equal-partition.cpp
+++ 250320/동일하게 분할/equal-partition.cpp
@@ -1,47 +1,54 @@
-// 총 합이 2n이라고 하면 n, n으로 나뉘어야하는데
-// 일단 순서는 상관이 없고, 순서를 보지 않으니 dp가 필요한가?
-
 #include <iostream>
 #include <vector>
-#include <algorithm>
-
 using namespace std;
 
-
-int main(){
-    int n, total = 0;
+int main() {
+    int n;
     cin >> n;
-
     vector<int> vec(n);
-    vector<int> dp(n+1, 0);
+    int total = 0;
     for (int i = 0; i < n; i++){
         cin >> vec[i];
         total += vec[i];
     }
-
-    sort(vec.begin(), vec.end());
-
-    bool flag = false;
-
-    for (int i = 1; i <= n; i++){
-        for (int j = 0; j < i; j++){
-            if (dp[j] + vec[i] == total/2){
-                flag = true;
-                break;
-            }
-            if (j == i-1){
-                dp[i] = dp[j] + vec[i];
+    
+    // 총합이 홀수면 두 그룹으로 똑같이 나누는 것은 불가능합니다.
+    if (total % 2 != 0) {
+        cout << "No";
+        return 0;
+    }
+    
+    int half = total / 2;
+    // dp[i][j] : 첫 i개의 원소를 사용해서 합 j를 만들 수 있으며, 그때 사용한 원소 개수를 저장합니다.
+    // 만들 수 없는 경우 -1을 저장합니다.
+    vector<vector<int>> dp(n + 1, vector<int>(half + 1, -1));
+    
+    // 0개의 원소로 합 0을 만드는 것은 가능하며, 사용한 원소 개수는 0입니다.
+    dp[0][0] = 0;
+    
+    // 각 원소에 대해 포함/미포함 경우를 고려합니다.
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= half; j++) {
+            if (dp[i][j] != -1) {
+                // 원소 i를 사용하지 않는 경우: 원래 값 유지
+                if (dp[i + 1][j] < dp[i][j])
+                    dp[i + 1][j] = dp[i][j];
+                
+                // 원소 i를 포함하는 경우 (합이 half를 넘지 않는다면)
+                if (j + vec[i] <= half) {
+                    int newCount = dp[i][j] + 1;
+                    if (dp[i + 1][j + vec[i]] < newCount)
+                        dp[i + 1][j + vec[i]] = newCount;
+                }
             }
         }
-        if (flag) break;
     }
-
-    if (total % 2 == 1 || !flag) cout << "No";
-    else cout << "Yes";
-
-
+    
+    // dp[n][half]가 0이면 빈 집합인 경우이고, n이면 전체 집합을 사용한 경우이므로 두 경우 모두 제외해야 합니다.
+    if (dp[n][half] > 0 && dp[n][half] < n)
+        cout << "Yes";
+    else
+        cout << "No";
+    
     return 0;
 }
-
-// 누적해서 더하는 방식을 갖고가면 어떨까?
-//
