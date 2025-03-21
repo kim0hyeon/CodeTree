7e2d87425a22e602e0a30bcce52cbf0fe40e940d
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-03-21 09:52:33 +0900

    Title: 대응되는 수와 문자, Time: 1029ms, Memory: 2MB, Status: Time Limit Exceed - CodeTree

---
+++ 250321/대응되는 수와 문자/corresponding-numbers-and-characters.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <stdexcept>
+
+using namespace std;
+
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+
+    string s;
+    unordered_map<string, int> umap;
+
+    for (int i = 1; i <= n; i++){
+        cin >> s;
+        umap[s] = i;
+    }
+
+    // 정수나 문자열 중 하나를 받게 되는데 일단 string으로 받고 그 다음에 숫자가 되는지 확인해야하나?
+    for (int i = 0; i < m; i++){
+        string input;
+        cin >> input;
+
+        try{
+            int num = stoi(input);
+            for (auto &pair : umap){
+                if (pair.second == num)
+                    cout << pair.first << endl;
+            }
+        } catch (const invalid_argument& e){
+            cout << umap[input] << endl;
+        }
+    }
+
+
+    return 0;
+}
