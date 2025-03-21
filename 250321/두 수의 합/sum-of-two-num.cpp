6f6a8951ce800d27f963e9c29ff722e15772c699
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-03-21 10:25:51 +0900

    Title: 두 수의 합, Time: 42ms, Memory: 2MB, Status: Passed - CodeTree

--- 250321/두 수의 합/sum-of-two-num.cpp
+++ 250321/두 수의 합/sum-of-two-num.cpp
@@ -1,33 +1,33 @@
 #include <iostream>
 #include <unordered_map>
-
 using namespace std;
-
 
 int main(){
     int n, m;
     cin >> n >> m;
-
-    unordered_map<int, int> umap;
-
+    
+    unordered_map<int, long long> freq;
     int num;
     for (int i = 0; i < n; i++){
         cin >> num;
-        umap[num] += 1;
+        freq[num]++;
     }
-
-    // 이렇게 풀면 시간 초과. 시간 복잡도를 줄일 방법이 필요한데. 맵을 만들 때 작업이 필요할것같은데...
-    int answer = 0;
-    for (auto &pair : umap){
-        if (umap.find(m-pair.first) != umap.end()){
-            answer += pair.second * umap[m-pair.first];
-            umap[pair.first] = 0;
-            umap[m-pair.first] = 0;
+    
+    long long answer = 0;
+    for(auto &p : freq){
+        int x = p.first;
+        int y = m - x;
+        if(freq.find(y) != freq.end()){
+            if(x == y){
+                // 같은 숫자일 경우, 조합으로 계산
+                answer += p.second * (p.second - 1LL) / 2;
+            }
+            else if(x < y){ // 중복 계산 방지
+                answer += p.second * freq[y];
+            }
         }
     }
-
+    
     cout << answer;
-
     return 0;
 }
-
