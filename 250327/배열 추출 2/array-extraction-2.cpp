96809bca247ce9d7ffc14a850924a1234eaadbcf
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-03-27 10:36:25 +0900

    Title: 배열 추출 2, Time: 91ms, Memory: 1MB, Status: Passed - Codetree

--- 250327/배열 추출 2/array-extraction-2.cpp
+++ 250327/배열 추출 2/array-extraction-2.cpp
@@ -8,39 +8,24 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    priority_queue<int> plus_q;
-    priority_queue<int> minus_q;
+    priority_queue<pair<int, int>> q;
 
     for (int i = 0; i < n; i++){
         int a;
         cin >> a;
 
         if (a != 0){
-            if (a > 0){
-                plus_q.push(-a);
-            } else{
-                minus_q.push(a);
-            }
+            q.push(make_pair(-abs(a), -a));
             continue;
         }
 
-        if (plus_q.empty() && !minus_q.empty()){
-            cout << minus_q.top() << endl;
-            minus_q.pop();
-        } else if (!plus_q.empty() && minus_q.empty()){
-            cout << -plus_q.top() << endl;
-            plus_q.pop();
-        } else if (!plus_q.empty() && !minus_q.empty()){
-            if (abs(minus_q.top()) <= abs(plus_q.top())){
-                cout << minus_q.top() << endl;
-                minus_q.pop();
-            } else{
-                cout << -plus_q.top() << endl;
-                plus_q.pop();
-            }
-        } else{
+        if (q.empty()){
             cout << 0 << endl;
+            continue;
         }
+        
+        cout << -q.top().second << endl;
+        q.pop();
     }
 
 
