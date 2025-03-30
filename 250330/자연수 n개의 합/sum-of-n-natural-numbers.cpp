c535ab6b2783c0c33763e297c8531ec7e1b914cd
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-03-30 23:12:18 +0900

    Title: 자연수 n개의 합, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250330/자연수 n개의 합/sum-of-n-natural-numbers.cpp
+++ 250330/자연수 n개의 합/sum-of-n-natural-numbers.cpp
@@ -12,7 +12,7 @@ int main(){
     while (left <= right){
         long long mid = (left + right) / 2;
 
-        if (mid*(mid+1)/2 > s){
+        if (mid > 2 * s / (mid+1)){
             right = mid-1;
         } else{
             max_num = max(max_num, mid);
