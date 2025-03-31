d8eabdef745ad1e96f17e6d58e7b469799b2cbff
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-03-31 14:14:25 +0900

    Title: 삼 오 무, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250331/삼 오 무/three-five-moo.cpp
+++ 250331/삼 오 무/three-five-moo.cpp
@@ -1,23 +1,43 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
 using namespace std;
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+// 변수 선언
+long long n;
 
-    long long N;
-    cin >> N;
+long long GetNumOfNumbers(long long mid) {
+    // moo의 수를 세어줍니다.
+    long long moo_cnt = mid / 3 + mid / 5 - mid / 15;
 
-    // 1~15 중 3,5의 배수가 아닌 수들 (오름차순)
-    static const vector<long long> valid = {1,2,4,7,8,11,13,14};
+    // 전체 수(mid)에서 moo의 수를 빼면
+    // 서로 다른 수의 개수가 구해집니다.
+    return mid - moo_cnt;
+}
 
-    // 몇 번째 블록인지 (0-based)
-    // (N-1)개를 8로 나눈 몫이 곧 블록 수, 나머지가 인덱스
-    long long block = (N - 1) / 8; 
-    long long r = (N - 1) % 8;
+int main() {
+    // 입력:
+    cin >> n;
 
-    long long answer = 15LL * block + valid[r];
-    cout << answer << "\n";
+    // 이 문제에서는
+    // [1 ~ K]까지 적혀있는 서로 다른 수의 개수가
+    // N 이상인 경우 중 가능한 K의 최솟값을 구하면 답이 됩니다.
 
+    long long left = 1;                          // 가장 작은 숫자 값을 설정합니다.
+    long long right = INT_MAX;                   // 가장 큰 숫자 값을 설정합니다.
+    long long min_num = INT_MAX;                 // 최소이므로, 답이 될 수 있는 값 중 최댓값으로 설정합니다.
+
+    while (left <= right) {                      // [left, right]가 유효한 구간이면 계속 수행합니다.
+        long long mid = (left + right) / 2;      // 가운데 위치를 선택합니다.
+        if(GetNumOfNumbers(mid) >= n) {          // 1부터 mid까지의 서로 다른 수의 개수가 n보다 같거나 크다면
+            right = mid - 1;                     // 왼쪽에 조건을 만족하는 숫자가 더 있을 가능성 때문에 right를 바꿔줍니다.
+            min_num = min(min_num, mid);         // 답의 후보들 중 최솟값을 계속 갱신해줍니다.
+        }
+        else                               
+            left = mid + 1;                      // 그렇지 않다면 left를 바꿔줍니다.
+    }
+
+    cout << min_num;                             // 조건을 만족하는 최솟값을 반환합니다.
     return 0;
 }
