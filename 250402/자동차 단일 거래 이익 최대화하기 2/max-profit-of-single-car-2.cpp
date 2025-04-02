c6160ddf41d2ad757b4915aa57e18b36cd61c839
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-04-02 15:57:18 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 17ms, Memory: 0MB, Status: Passed - Codetree

--- 250402/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.cpp
+++ 250402/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.cpp
@@ -9,14 +9,13 @@ int main(){
     int n;
     cin >> n;
     int arr[n];
-
     for (int i = 0; i < n; i++) cin >> arr[i];
 
     // 그 다음 녀석이 이후까지의 녀석중에 최대를 정하면 되는데
     int dp[n];
     dp[n-1] = arr[n-1];
     for (int i = n-2; i >= 0; i--){
-        dp[i] = max(arr[i+1], arr[i]);
+        dp[i] = max(dp[i+1], arr[i]);
     }
 
     int max_val = INT_MIN;
