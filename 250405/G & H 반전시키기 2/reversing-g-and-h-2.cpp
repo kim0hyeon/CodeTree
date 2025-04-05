430ab18465039b044ade2233d033fa177e1f157e
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-04-05 22:47:02 +0900

    Title: G & H 반전시키기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250405/G & H 반전시키기 2/reversing-g-and-h-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+
+    string origin, target;
+    cin >> origin >> target;
+
+    int count = 0;
+    for (int i = n-1; i > 0; i--){
+        if (origin[i] != target[i]){
+            for (int j = 0; j <= i; j++){
+                origin[j] == 'G' ? origin[j] = 'H' : origin[j] = 'G';
+            }
+            count++;
+        }
+    }
+
+    cout << count;
+
+    return 0;
+}
