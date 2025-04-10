a42bb42669da5a168f7507adf4fe621706a24737
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-04-10 17:20:09 +0900

    Title: 제곱하여 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250410/제곱하여 출력하기/print-square-of-elements.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    for (int k : arr) cout << k*k << " ";
+
+    return 0;
+}
