222cad924564556acbc22641858964f46bae7685
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-04-10 16:38:38 +0900

    Title: 실수 받아 그대로 출력, Time: 5ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250410/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    double n;
+    cin >> n;
+
+    cout << fixed << setprecision(2);
+    cout << n;
+    return 0;
+}
