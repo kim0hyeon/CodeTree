89f43c1765d1d7de2405e61397a49c30ca8df5ca
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-04-10 16:47:49 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250410/비교 연산/comparison-operator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    a >= b ? cout << 1 << endl : cout << 0 << endl;
+    a > b ? cout << 1 << endl : cout << 0 << endl;
+    a <= b ? cout << 1 << endl : cout << 0 << endl;
+    a < b ? cout << 1 << endl : cout << 0 << endl;
+    a == b ? cout << 1 << endl : cout << 0 << endl;
+    a != b ? cout << 1 << endl : cout << 0 << endl;
+
+    return 0;
+}
