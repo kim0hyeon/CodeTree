ee42442a06b51b8d7dd8c37fe47c12a6af7d84d6
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-04-10 17:46:47 +0900

    Title: 특정 수 맞히기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250410/특정 수 맞히기/catching-specific-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    while (true){
+        int n;
+        cin >> n;
+
+        if (n > 25) cout << "Lower" << endl;
+        else if (n < 25) cout << "Higher" << endl;
+        else {cout << "Good" << endl; break;}
+    }
+    return 0;
+}
