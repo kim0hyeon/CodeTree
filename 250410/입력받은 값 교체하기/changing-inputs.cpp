a32d86a441a4006e6c0aa20f6ffdae1a2642e939
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-04-10 16:40:11 +0900

    Title: 입력받은 값 교체하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250410/입력받은 값 교체하기/changing-inputs.cpp
+++ 250410/입력받은 값 교체하기/changing-inputs.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <algorithm>
+
 using namespace std;
 
 int main() {
     int a, b;
     cin >> a >> b;
-    cout << b << a;
+    swap(a, b);
+
+    cout << a << " " << b;
     return 0;
 }
