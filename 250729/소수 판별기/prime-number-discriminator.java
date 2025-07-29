0f80d7b79ce891f0ec3e50bde50bf2abb5a22069
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-07-29 21:55:07 +0900

    Title: 소수 판별기, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

--- 250729/소수 판별기/prime-number-discriminator.java
+++ 250729/소수 판별기/prime-number-discriminator.java
@@ -7,7 +7,7 @@ public class Main {
         int n = sc.nextInt();
         boolean flag = true;
 
-        for (int i = 2; i < n; i++){
+        for (int i = 2; i < n/2; i++){
             if (n%i == 0){
                 flag = false;
                 break;
