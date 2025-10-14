416a91ad06b77f55d3b791e29f1331b6251424d2
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2025-10-14 11:16:20 +0900

    Title: 두 배열의 곱, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 251014/두 배열의 곱/multiple-of-two-arrays.py
+++ 251014/두 배열의 곱/multiple-of-two-arrays.py
@@ -1,7 +1,6 @@
 arr1 = [list(map(int, input().split())) for _ in range(3)]
 s = input()
 arr2 = [list(map(int, input().split())) for _ in range(3)]
-s = input()
 
 for i in range(3):
     for j in range(3):
