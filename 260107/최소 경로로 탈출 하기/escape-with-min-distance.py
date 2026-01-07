35a7cb62380fbfa2f29ce793360767cdfd48e68a
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2026-01-07 20:19:55 +0900

    Title: 최소 경로로 탈출 하기, Time: 88ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 260107/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+dp = [[101 for _ in range(m)] for _ in range(n)]
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def BFS():
+    q = deque()
+    q.append((0, 0))
+    dp[0][0] = 0
+
+    while len(q) != 0:
+        point = q.pop()
+        x, y = point[0], point[1]
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx >= 0 and nx < n and ny >= 0 and ny < n:
+                if grid[nx][ny] != 0 and dp[x][y] + 1 < dp[nx][ny]:
+                    dp[nx][ny] = dp[x][y] + 1
+                    q.append((nx, ny))
+
+BFS()
+if dp[n-1][n-1] == 101:
+    print(-1)
+else:
+    print(dp[n-1][n-1])
