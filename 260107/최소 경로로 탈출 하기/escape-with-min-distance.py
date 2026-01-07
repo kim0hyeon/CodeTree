5daeb80cf5cafaa4fb93b9c2b56447c3cf151a4f
kim0hyeon <114048115+kim0hyeon@users.noreply.github.com>
2026-01-07 20:29:46 +0900

    Title: 최소 경로로 탈출 하기, Time: 76ms, Memory: 18MB, Status: Wrong Answer - Codetree

--- 260107/최소 경로로 탈출 하기/escape-with-min-distance.py
+++ 260107/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -4,7 +4,7 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
-dp = [[101 for _ in range(m)] for _ in range(n)]
+dp = [[201 for _ in range(m)] for _ in range(n)]
 
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
@@ -15,19 +15,19 @@ def BFS():
     dp[0][0] = 0
 
     while len(q) != 0:
-        point = q.pop()
+        point = q.popleft()
         x, y = point[0], point[1]
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if nx >= 0 and nx < n and ny >= 0 and ny < n:
+            if nx >= 0 and nx < n and ny >= 0 and ny < m:
                 if grid[nx][ny] != 0 and dp[x][y] + 1 < dp[nx][ny]:
                     dp[nx][ny] = dp[x][y] + 1
                     q.append((nx, ny))
 
 BFS()
-if dp[m-1][n-1] == 101:
+if dp[n-1][m-1] == 201:
     print(-1)
 else:
-    print(dp[m-1][n-1])
+    print(dp[n-1][m-1])
